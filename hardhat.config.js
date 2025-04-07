0576ca726ac0aad5fb31f88867dda9e340c86ce7
Anirudh <aanirudh7777@gmail.com>
2025-04-07 16:20:07 +0530

    additional networks removed from hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,14 +6,6 @@ const privateKey = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   networks: {
-    hardhat: {
-      chainId: 31337,
-    },
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      chainId: 31337, // Explicitly set to match Hardhat node
-      accounts: [privateKey], // Optional: Use for deployment
-    },
     amoy: {
       url: "https://virtual.polygon-amoy.rpc.tenderly.co/bb75e8d9-b9c8-4de8-8032-18917de2b92b",
       accounts: [privateKey],
